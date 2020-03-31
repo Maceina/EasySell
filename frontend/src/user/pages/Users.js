3b7ad79affcfbe90f9a8715b169db26994cdf921
Your Name <you@example.com>
2020-03-31 17:02:20 +0300

    Creating users list

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
+
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-    return <h2>Users works!</h2>
-}
+  const USERS = [
+    {
+      id: "u1",
+      name: "Max Swartz",
+      image:
+        "https://img.huffingtonpost.com/asset/5dcc613f1f00009304dee539.jpeg?cache=QaTFuOj2IM&ops=crop_834_777_4651_2994%2Cscalefit_720_noupscale&format=webp",
+      places: 3
+    }
+  ];
+
+  return <UsersList items={USERS} />;
+};
 
 export default Users;
