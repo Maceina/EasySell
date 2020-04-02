1039815400f7d879fb53f32627816cd3ff7bcb48
Your Name <you@example.com>
2020-04-02 23:33:25 +0300

    fucking validator ffs

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useReducer } from "react";
+import React from "react";
 import {
   VALIDATOR_REQUIRE,
   VALIDATOR_MINLENGTH
@@ -6,36 +6,13 @@ import {
 
 import Button from "../../shared/components/FormElements/Button";
 
-import "./NewPlace.css";
+import "./PlaceForm.css";
 import Input from "../../shared/components/FormElements/Input";
-
-const formReducer = (state, action) => {
-  switch (action.type) {
-    case "INPUT_CHANGE":
-      let formIsValid = true;
-      for (const inputId in state.inputs) {
-        if (inputId === action.inputId) {
-          formIsValid = formIsValid && action.isValid;
-        } else {
-          formIsValid = formIsValid && state.inputs[inputId].isValid;
-        }
-      }
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.inputId]: { value: action.value, isValid: action.isValid }
-        },
-        isValid: formIsValid
-      };
-    default:
-      return state;
-  }
-};
+import { useForm } from "../../shared/hooks/form-hook";
 
 const NewPlace = () => {
-  const [formState, dispatch] = useReducer(formReducer, {
-    inputs: {
+  const [formState, inputHandler] = useForm(
+    {
       title: {
         value: "",
         isValid: false
@@ -43,23 +20,19 @@ const NewPlace = () => {
       description: {
         value: "",
         isValid: false
+      },
+      address: {
+        value: "",
+        isValid: false
       }
     },
-    isValid: false
-  });
+    false
+  );
 
-  const inputHandler = useCallback((id, value, isValid) => {
-    dispatch({
-      type: "INPUT_CHANGE",
-      value: value,
-      isValid: isValid,
-      inputId: id
-    });
-  }, []);
 
   const placeSubmitHandler = event => {
-      event.preventDefault();
-      console.log(formState.inputs); // send this to the backend!
+    event.preventDefault();
+    console.log(formState.inputs); // send this to the backend!
   };
 
   return (
